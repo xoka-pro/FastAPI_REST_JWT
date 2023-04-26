06edba62b94540287875c450620505b01d4a0a77
Oleh Ovchinnikov <112849429+xoka-pro@users.noreply.github.com>
2023-04-26 11:58:03 +0300

    add defaults for pydentic settings

--- src/conf/config.py
+++ src/conf/config.py
@@ -2,21 +2,22 @@ from pydantic import BaseSettings
 
 
 class Settings(BaseSettings):
-    sqlalchemy_database_url: str
-    secret_key: str
-    algorithm: str
-    mail_username: str
-    mail_password: str
-    mail_from: str
-    mail_port: int
-    mail_server: str
-    redis_host: str
-    redis_port: int
-    cloudinary_name: str
-    cloudinary_api_key: int
-    cloudinary_api_secret: str
+    sqlalchemy_database_url: str = 'postgresql+psycopg2://user:password@localhost:5432/postgres'
+    = 'secret_key'
+    algorithm: str = 'HS256'
+    mail_username: str = 'example@meta.ua'
+    mail_password: str = 'password'
+    mail_from: str = 'example@meta.ua'
+    mail_port: int = 465
+    mail_server: str = 'smtp.meta.ua'
+    redis_host: str = 'localhost'
+    redis_port: int = 6379
+    cloudinary_name: str = 'name'
+    cloudinary_api_key: int = 123456789012345
+    cloudinary_api_secret: str = 'secret'
 
-    class Config:
+
+class Config:
         env_file = ".env"
         env_file_encoding = "utf-8"
 

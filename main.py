991421797a91e748300ba8505544ad9c7b9d2253
Oleh Ovchinnikov <112849429+xoka-pro@users.noreply.github.com>
2023-04-26 12:00:32 +0300

    add dock-strings

--- main.py
+++ main.py
@@ -17,6 +17,14 @@ app = FastAPI()
 
 @app.on_event("startup")
 async def startup():
+    """
+    The startup function is called when the application starts up.
+    It's a good place to initialize things that are needed by your app,
+    like connecting to databases or initializing external APIs.
+
+    :return: The FastAPILimiter object
+    :doc-author: Trelent
+    """
     r = await redis.Redis(host='localhost', port=6379, db=0, encoding="utf-8", decode_responses=True)
     await FastAPILimiter.init(r)
 
@@ -32,6 +40,15 @@ app.add_middleware(
 
 @app.middleware('http')
 async def custom_middleware(request: Request, call_next):
+    """
+    The custom_middleware function is a middleware function that adds the time it took to process
+    the request in seconds as a header called 'performance'
+
+    :param request: Request: Get the request object
+    :param call_next: Call the next middleware in the chain
+    :return: A middleware function
+    :doc-author: Trelent
+    """
     start_time = time.time()
     response = await call_next(request)
     during = time.time() - start_time
@@ -41,11 +58,27 @@ async def custom_middleware(request: Request, call_next):
 
 @app.get("/", dependencies=[Depends(RateLimiter(times=2, seconds=5))])
 async def root():
+    """
+    The root function is a simple function that returns a welcome message.
+    It's purpose is to show the user how to use the API and what it can do.
+
+    :return: A dictionary with a key &quot;message&quot; and value &quot;welcome to restapi&quot;
+    :doc-author: Trelent
+    """
     return {"message": "Welcome to RESTapi"}
 
 
 @app.get("/api/healthchecker")
 def healthchecker(db: Session = Depends(get_db)):
+    """
+    The healthchecker function is a simple function that checks the health of the database.
+    It does this by making a request to the database and checking if it returns any results.
+    If there are no results, then we know something is wrong with our connection.
+
+    :param db: Session: Get the database connection from the dependency
+    :return: A dictionary with a message
+    :doc-author: Trelent
+    """
     try:
         # Make request
         result = db.execute(text("SELECT 1")).fetchone()
